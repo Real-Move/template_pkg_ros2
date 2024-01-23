93a08b2bf4d8e28a348220c5d9db5d0005b5e49b
Luca Fortini <lucafortini60@gmail.com>
2024-01-23 16:40:39 +0100

    Added generic docker

---
+++ .docker/ros_entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function ros_source_env() 
+{
+	if [ -f "$1" ]; then
+		echo "sourcing   $1"
+		source "$1"
+	else
+		echo "notfound   $1"
+	fi	
+}
+
+ros_source_env "$ROS_ROOT/setup.bash"
+
+exec "$@"
