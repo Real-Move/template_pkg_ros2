75195e185dcd81ba2330721ddfa311a32b54e4a0
Luca Fortini <lucafortini60@gmail.com>
2024-02-23 12:13:08 +0100

    minor changes

--- launch/include.launch.py
+++ launch/include.launch.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) 2024 Luca Fortini
+# Copyright (c) 2024 Real Move
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
